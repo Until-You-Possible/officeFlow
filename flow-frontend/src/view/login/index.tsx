8f4d7201578f05004f33639f5f3deb4d7a2749ce
ray <gang.wang05@sap.com>
2022-07-10 23:54:42 +0800

    add: 1：调整结构，学习使用MaterialUI搭建登陆页面(1)

--- flow-frontend/src/view/login/index.tsx
+++ flow-frontend/src/view/login/index.tsx
@@ -1,16 +1,47 @@
 import React from "react";
 import loginStyle from "../../css/login.module.css";
 import {
+    Avatar,
+    Box,
     Button,
     Card,
     CardActions,
     CardContent,
     CardHeader,
-    Divider, FormControl, Input, InputAdornment, InputLabel, TextField,
+    Divider, FormControl, Grid, Input, InputAdornment, InputLabel, Paper, styled, TextField,
     Typography
 } from "@mui/material";
 
-import {AccountCircle} from "@mui/icons-material";
+function MarginBar() {
+    return (
+        <Box
+            sx={{
+                height: 30,
+                backgroundColor: (theme: any) =>
+                    theme.palette.mode === 'light'
+                        ? 'rgba(255, 255, 255, 1)'
+                        : 'rgb(255 132 132 / 25%)',
+            }}
+        />
+    );
+}
+
+function Choice() {
+    return (
+        <Box
+            sx={{
+                height: 50,
+                textAlign: 'center',
+                lineHeight: "50px",
+                color: (theme: any) =>
+                    theme.palette.mode === 'light'
+                        ? 'rgba(0, 0, 0, .4)'
+                        : 'rgb(255 132 132 / 25%)',
+            }}
+        >OR</Box>
+    );
+}
+
 
 
 const Login: React.FC = () => {
@@ -22,34 +53,41 @@ const Login: React.FC = () => {
     return (
         <>
             <div className={loginStyle.container}>
+             <Card className={loginStyle.MRoot}>
+                <CardContent>
+                    <Typography className={loginStyle.MTypographyTitle}
+                                color="text.secondary"
+                                gutterBottom >
+                        log in to your account
+                    </Typography>
+                    <MarginBar />
+                    <TextField
+                        label="username"
+                        fullWidth
+                        required
+                        id="outlined-name"
+                        color="secondary"
+                        value=""
+                        placeholder="please input your username"
+                    />
+                    <MarginBar />
+                    <TextField
+                        label="password"
+                        fullWidth
+                        required
+                        color="secondary"
+                        id="outlined-name"
+                        value=""
+                        placeholder="please input your password"
+                    />
+                    <MarginBar />
+                    <Button fullWidth variant="contained" size="large">Log in</Button>
+                    <Choice />
+                    <Button fullWidth variant="outlined" size="large">Log in with wechat</Button>
 
-                <Card className={loginStyle.MRoot}>
-                    <CardHeader title="Login" className={loginStyle.MHead} />
-                    <Divider variant="middle" />
-                    <CardContent>
+                </CardContent>
+            </Card>
 
-                        <TextField
-                            id="outlined-name"
-                            label="Username"
-                            size="small"
-                            value="333"
-                        />
-
-                        <TextField
-                            id="outlined-name"
-                            label="Password"
-                            size="small"
-                            value="333"
-                        />
-
-                    </CardContent>
-                    <Divider variant="middle" />
-                    <CardActions className={loginStyle.MAction}>
-                        <Button variant="contained" color="primary" className={loginStyle.MButton}>
-                            continue
-                        </Button>
-                    </CardActions>
-                </Card>
 
             </div>
         </>
